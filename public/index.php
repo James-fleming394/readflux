e8490d7ee73718e47faa20e482f6589135a1db7b
james-fleming394 <james.fleming394@gmail.com>
2025-06-20 13:15:11 -0400

    Add: Entry point Public index.php set up.

---
+++ public/index.php
@@ -0,0 +1,18 @@
+<?php
+// Enable error reporting for development
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+
+require_once '../config/config.php';
+require_once '../app/core/Router.php';
+require_once '../app/core/Controller.php';
+
+// Autoload controllers
+spl_autoload_register(function ($class) {
+    if (file_exists("../app/controllers/$class.php")) {
+        require_once "../app/controllers/$class.php";
+    }
+});
+
+$router = new \App\Core\Router();
+$router->dispatch($_SERVER['REQUEST_URI']);
