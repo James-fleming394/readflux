0b4b7bf8fbacfdbdd5f40411b4bcebf110daace2
james-fleming394 <james.fleming394@gmail.com>
2025-06-21 11:27:03 -0400

    Add: Config.php added to config folder for DB set up

---
+++ config/config.php
@@ -0,0 +1,10 @@
+<?php
+
+// Database settings
+define('DB_HOST', 'localhost');
+define('DB_NAME', 'readflux');
+define('DB_USER', 'root');
+define('DB_PASS', ''); // add password if needed
+
+// Base URL (optional)
+define('BASE_URL', 'http://localhost:8000');
