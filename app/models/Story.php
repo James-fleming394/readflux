33ffc0d60207d53013c9e18f5d7c8c40d2e366b5
james-fleming394 <james.fleming394@gmail.com>
2025-06-29 20:32:56 -0400

    Add: Story create and getAll in Story Model

--- app/models/Story.php
+++ app/models/Story.php
@@ -15,4 +15,15 @@ class Story
         $stmt = $this->db->query("SELECT * FROM stories ORDER BY created_at DESC");
         return $stmt->fetchAll(PDO::FETCH_ASSOC);
     }
+
+    public function create($title, $body, $userId)
+    {
+        $stmt = $this->db->prepare("INSERT INTO stories (title, body, user_id) VALUES (:title, :body, :user_id)");
+        $stmt->execute([
+            ':title' => $title,
+            ':body' => $body,
+            ':user_id' => $userId
+    ]);
+    }
+
 }
