200070856cac0126ba6fccfd51784921ca87a7db
james-fleming394 <james.fleming394@gmail.com>
2025-06-29 20:29:41 -0400

    Add: Story create and store in StoryController

--- app/controllers/StoryController.php
+++ app/controllers/StoryController.php
@@ -9,4 +9,30 @@ class StoryController extends \App\Core\Controller
 
         $this->view('story/index', ['stories' => $stories]);
     }
+
+        public function create()
+    {
+        $this->view('story/create');
+    }
+
+    public function store()
+    {
+        require_once '../app/models/Story.php';
+        $storyModel = new Story();
+
+        // Fake user ID for now
+        $userId = 1;
+
+        $title = $_POST['title'] ?? '';
+        $body = $_POST['body'] ?? '';
+
+        if (!empty($title) && !empty($body)) {
+            $storyModel->create($title, $body, $userId);
+            header("Location: " . BASE_URL . "/story");
+            exit;
+        } else {
+            echo "<p>Please fill in all fields.</p>";
+        }
+    }
+
 }
